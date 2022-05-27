fa4d134444a2337019ea4d5479fc7cdcddba2c34
jphavill <jphavill@gmail.com>
2022-05-27 14:56:40 -0300

    working vector based animation, without interpolation

--- MusicVis.py
+++ MusicVis.py
@@ -4,13 +4,14 @@ from celluloid import Camera
 import time
 import numpy as np
 from random import shuffle
+import bisect
 
 import pandas as pd
 from itunes_reader import create_musicdf
 
 from matplotlib.animation import PillowWriter
 
-INTERP_FRAMES = 15
+INTERP_FRAMES_PER_MONTH = 3
 MIN_PCT = 1
 COLORMAP = 'tab20c'
 FONTSIZE = 13
@@ -40,22 +41,6 @@ def song_duration_start_stop(row: pd.Series) -> int:
     else:
       return row['Total Time'] * row['Play Count']
 
-
-def count_month_plays(year_month: str):
-  return music_df[START_DATE <= music_df['sort_date'] <= year_month]['Play Count'].sum()
-
-def count_month_duration(year_month: str):
-  return music_df[START_DATE <= music_df['sort_date'] <= year_month]['duration'].sum()
-
-
-def sum_total_duration() -> int:
-  return music_df['duration'].sum()
-
-
-def artist_duration(artist: str) -> int:
-  return music_df[music_df['Artist'] == artist]['duration'].sum()
-
-
 '''
 I think don't do iteratively by row right away, first do group by to group of year and artist sums which should give every artists sum per year'''
 
@@ -81,7 +66,10 @@ def create_date_plays_table(start_date: str, end_date: str) -> pd.DataFrame:
   # running summation of the songs per month block
   table = table.cumsum()
   # WE DID IT LETS GOOOOOOOOOOOOOOOO!!!!!!!!!!!!
-  return table
+  # table = table.set_index('sort_date', 'total_duration')
+  tablepct = table.div(table.total_duration, axis=0)
+  tablepct['total_duration'] = table.total_duration
+  return tablepct
 
 def format_duration(duration):
   days = duration // 86400
@@ -103,33 +91,22 @@ def count_month(year_month) -> tuple:
   return artist_count, total, total_duration
 
 
-def create_data(artist_count, total):
-  xy = list(artist_count.items())
-  xy.sort(key=lambda x: x[1])
-  chart_labels = []
-  chart_sizes = []
-  other_total = 0
-  for key, value in xy:
-    if value/total > MIN_PCT/100:
-      chart_labels.append(key)
-      chart_sizes.append(value/total)
-    else:
-      other_total += value
-
-  other_value = other_total/total
-
+def create_data_vector(s: pd.Series) -> tuple:
+  s = s.drop('total_duration')
+  others = s[s < 0.01]
+  other_value = others.sum()
+  not_others = s[s > 0.01]
+  chart_labels = list(not_others.index)
+  chart_sizes = list(not_others.values)
   if other_value > MIN_PCT/100:
-    i = 0
-    for i in range(len(chart_sizes)):
-      if chart_sizes[i] > other_value:
-        i -= 1
-        break
-      i += 1
+    others_index = bisect.bisect(chart_sizes, other_value)
 
-    chart_sizes.insert(i+1, other_value)
-    chart_labels.insert(i + 1, f'Other < {MIN_PCT}%')
+    chart_sizes.insert(others_index, other_value)
+    chart_labels.insert(others_index, f'Other < {MIN_PCT}%')
+  # sort the wedge labels and size by size of section
+  chart_sizes, chart_labels = zip(*sorted(zip(chart_sizes, chart_labels)))
+  chart_explosion = [0.0007 / i ** (1.5) for i in chart_sizes]
 
-  chart_explosion = [0.0007 / i**(1.5) for i in chart_sizes]
   return chart_sizes, chart_labels, chart_explosion
 
 
@@ -137,82 +114,32 @@ def interp(x1, x2, interp_frames, i):
   return x1 + (x2-x1)/interp_frames * i
 
 
-def annotate_time(year_month, duration_total, chart_sizes, chart_labels, chart_explosion):
+'''returns the number of months between year_month dates'''
+def year_month_diff(ym1: str, ym2: str) -> int:
+  return (int(ym2[:4]) * 12 + int(ym2[4:])) - (int(ym1[:4]) * 12 + int(ym1[4:]))
+
+
+def animate_frame(year_month, duration_total, colours, chart_sizes, chart_labels, chart_explosion):
   ax1.annotate(f'{year_month[:4]} - {NUM_TO_MONTHS[int(year_month[4:])]}', (1, -1), fontSize=30)
   ax1.annotate(f'{format_duration(duration_total)}', (0.45, 1.55), fontSize=16)
   ax1.annotate('Total Listening Time', (0.48, 1.65), fontSize=25)
   pie_wedges = ax1.pie(chart_sizes, labels=chart_labels, explode=chart_explosion, autopct='%1.1f%%', startangle=90,
                        rotatelabels=True, textprops={'fontsize': FONTSIZE}, labeldistance=1.05)[0]
   for wedge in pie_wedges:
-    wedge.set_facecolor(colors[wedge.get_label()])
+    wedge.set_facecolor(colours[wedge.get_label()])
   ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
   camera.snap()
 
 
+def plt_data_vector(s: pd.Series, colours):
+  year_month = s.name
+  chart_sizes, chart_labels, chart_explosion = create_data_vector(s)
+  animate_frame(year_month, s["total_duration"], colours, chart_sizes, chart_labels, chart_explosion)
 
-def plt_data(ax1, camera, year_month, colors):
-  global future_month
-  global future_year
-  artist_count, total, duration_total = count_month(year_month)
-  chart_sizes, chart_labels, chart_explosion = create_data(artist_count, total)
-  # if its the last frame
+  # if its the last frame hold it for a total of 15 frames, half a second
   if year_month == END_DATE:
-    for i in range(15):
-      ax1.annotate(f'{year_month[:4]} - {NUM_TO_MONTHS[int(year_month[4:])]}', (1, -1), fontSize=30)
-      ax1.annotate(f'{format_duration(duration_total)}', (0.45, 1.55), fontSize=16)
-      ax1.annotate('Total Listening Time', (0.48, 1.65), fontSize=25)
-      pie_wedges = ax1.pie(chart_sizes, labels=chart_labels, explode=chart_explosion, autopct='%1.1f%%', startangle=90,
-              rotatelabels=True, textprops={'fontsize': FONTSIZE}, labeldistance=1.05)[0]
-      for wedge in pie_wedges:
-        wedge.set_facecolor(colors[wedge.get_label()])
-      ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-      camera.snap()
-  else:
-    year = int(year_month[:4])
-    month = int(year_month[4:])
-    if month <= future_month and year <= future_year:
-      return
-    next_month = (month + 1) % 13
-    interp_months = 1
-    next_year = year
-    if next_month == 0:
-      next_year = year + 1
-      next_month += 1
-    next_year_month = str(next_year) + str(next_month).zfill(2)
-    next_artist_count, next_total, next_duration_total = count_month(next_year_month)
-    while next_total < total+50 and int(next_year_month) < int(music_df['sort_date'].max() and next_month <= 4):
-      next_month += 1
-      future_month = next_month-1
-      future_year = next_year
-      interp_months += 1
-      next_artist_count, next_total, next_duration_total = count_month(next_year_month)
-
-    # adjust the values of artists counts
-
-    deltas = defaultdict(int)
-    for key in set(list(artist_count.keys()) + list(next_artist_count.keys())):
-      deltas[key] = next_artist_count[key] - artist_count[key]
-    total_interp_frames = INTERP_FRAMES * interp_months
-
-    for m in range(1, interp_months+1):
-      for i in range(1+(m-1)*INTERP_FRAMES,1+(m)*INTERP_FRAMES):
-        interp_artist_count = defaultdict(int)
-        for key, value in deltas.items():
-          interp_artist_count[key] = artist_count[key] + (deltas[key]/total_interp_frames) * i
-        interp_total = interp(total, next_total, total_interp_frames, i)
-        interp_chart_sizes, interp_labels, interp_chart_explosion = create_data(interp_artist_count, interp_total)
-        ax1.annotate(f'{year+ (1 if (month+m-1 > 12) else 0)} - {NUM_TO_MONTHS[(month+m-1)%13 + (1 if (month+m-1>12) else 0)]}', (1, -1), fontSize=30)
-        ax1.annotate(f'{format_duration(interp(duration_total, next_duration_total, total_interp_frames, i))}',(0.45, 1.55), fontSize=16)
-        ax1.annotate('Total Listening Time',
-                     (0.48, 1.65), fontSize=25)
-        pie_wedges = ax1.pie(interp_chart_sizes, labels=interp_labels, explode=interp_chart_explosion, autopct='%1.1f%%', startangle=90,
-                             rotatelabels=True, textprops={'fontsize': FONTSIZE}, labeldistance=1.05)[0]
-        for wedge in pie_wedges:
-          wedge.set_facecolor(colors[wedge.get_label()])
-        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-        camera.snap()
-
+    for i in range(14):
+      animate_frame(year_month, s["total_duration"], colours, chart_sizes, chart_labels, chart_explosion)
 
 music_df = create_musicdf(required_fields=('Date Added', 'Artist', 'Play Count', 'Start Time', 'Stop Time', 'Total Time'))
 start_time = time.time()
@@ -229,7 +156,7 @@ music_df.sort_values('sort_date')
 unique_dates = music_df['sort_date'].unique()
 unique_dates.sort()
 START_DATE = unique_dates[0]
-END_DATE = unique_dates[5]
+END_DATE = unique_dates[-1]
 
 # filter music data frame to only be the selected time span
 music_df = music_df.loc[(START_DATE <= music_df['sort_date']) & (music_df['sort_date'] <= END_DATE)]
@@ -260,20 +187,25 @@ camera = Camera(fig1)
 artists_used = set()
 
 # print(music_df.Artist.unique())
-print(create_date_plays_table(START_DATE, END_DATE).to_string())
+artist_df = create_date_plays_table(START_DATE, END_DATE)
+
+for index, row in artist_df.iterrows():
+    plt_data_vector(row, artist_colors)
+
+# print(artist_df.to_string())
 # # probably have to deal with the fact that unique won't have every month year combo
 # for date in unique_dates[5:31]:
 #   plt_data(ax1, camera, date, artist_colors)
 #
-# print(time.time() - start_time)
-# print('animating')
-# animation = camera.animate(interval=1000/30,blit=True)
-# # for mp4's code is 'celluloid_subplots.mp4'
-# # for debug's code is 'celluloid_subplots.gif', writer=PillowWriter(30)
-# animation.save('celluloid_subplots.gif', writer=PillowWriter(30))
-#
-# # prints the number of seconds it took to animate
+print(time.time() - start_time)
+print('animating')
+ani_start_time = time.time()
+animation = camera.animate(interval=1000/30,blit=True)
+# for mp4's code is 'celluloid_subplots.mp4'
+# animation.save('celluloid_subplots.mp4')
+# for debug's code is 'celluloid_subplots.gif', writer=PillowWriter(30)
+animation.save('celluloid_subplots.gif', writer=PillowWriter(30))
 
-
-
+# prints the number of seconds it took to animate
+print(time.time() - start_time)
 #interp by using enumerate instead of just iteration, get difference between this month and next and gradually adjust'''
